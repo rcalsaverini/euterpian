d66ad8fe9c1fe31b1f1909aacf77283d176198d6
Rafael Calsaverini <rafael.calsaverini@nubank.com.br>
2020-09-03 11:21:34 -0300

    chord tables started

--- euterpian/chords.py
+++ euterpian/chords.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from itertools import product
+from itertools import permutations
+from math import factorial
 from tqdm import tqdm
 from pychord.analyzer import get_all_rotated_notes, notes_to_positions
 from pychord.analyzer import find_quality
@@ -9,10 +10,10 @@ COMPLEXITY_CLASSES = {
     1: ["", "m", "5"],
     2: ["7", "m7"],
     3: ["M7", "mM7", "sus2", "sus4"],
-    4: ["6", "9", "7+9", "7-9", "7sus4", "9sus4", "m9", "m7-5", "add9", "M9"],
+    4: ["6", "9", "7+9", "7-9", "7sus4", "9sus4", "m9", "m6", "m7-5", "add9", "M9"],
     5: ["7+5", "7-5", "M7+5"],
     6: ["11", "add11", "7+11", "7-13"],
-    10: ["aug", "dim", "dim6"],
+    10: ["aug", "dim", "dim6", "7#9b5", "9-5", "9+5", "7#9#5", "7b9b5", "7b9#5"],
 }
 
 
@@ -56,14 +57,16 @@ def relative_complexity(scale):
     return complexity
 
 
-def iterate_scale(scale, times, verbose=0):
-    iterator = product(*[scale for _ in range(times)])
+def iterate_scale(scale, chord_size, verbose=0):
+    scale_length = len(scale)
+    iterator = permutations(scale, chord_size)
     if verbose == 0:
         yield from iterator
     else:
-        size = len(scale) ** times
-        desc = f"{scale}^{times}"
+        size = factorial(scale_length) * factorial(scale_length - chord_size)
+        desc = f"{scale}"
         yield from tqdm(iterator, desc=desc, total=size)
+
 
 
 def chords_from_notes(notes, inversions=False):
@@ -73,7 +76,18 @@ def chords_from_notes(notes, inversions=False):
             yield chord
 
 
+
 def chords_from_scale(scale, inversions=False, diversity=(2, 5), verbose=0):
+    min_diversity, max_diversity = diversity
+    complexity = relative_complexity(scale)
+    yield from (
+        chord
+        for k in range(min_diversity, max_diversity + 1)
+        for notes in iterate_scale(scale, k, verbose=verbose)
+        for chord in chords_from_notes(notes, inversions=inversions)
+    )
+
+def tabulate_chords_from_scale(scale, inversions=False, diversity=(2, 5), verbose=0):
     min_diversity, max_diversity = diversity
     complexity = relative_complexity(scale)
     return pd.DataFrame(
@@ -94,7 +108,7 @@ def chord_table(scale, inversions=False, diversity=(2, 5), verbose=0, index=None
         sorted_chords = sorted(list(xs), key=complexity)
         return ", ".join([chord.chord for chord in sorted_chords])
 
-    chords = chords_from_scale(scale, inversions=inversions, diversity=diversity, verbose=verbose)
+    chords = tabulate_chords_from_scale(scale, inversions=inversions, diversity=diversity, verbose=verbose)
     return chords.pivot_table(
         columns=columns or "complexity",
         index=index or "root",
